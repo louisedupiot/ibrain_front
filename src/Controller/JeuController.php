5f43404dacf515ade6f485a12d3dec63763f2f91
Ayoub Anzid <anzidayoub@outlook.fr>
2022-07-24 16:10:47 +0200

    Jeu démineur

--- src/Controller/JeuController.php
+++ src/Controller/JeuController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Process\Process;
+Use Symfony\Component\Process\Exception\ProcessFailedException;
+
 
 class JeuController extends AbstractController
 {
@@ -17,8 +21,19 @@ class JeuController extends AbstractController
     }
 
     #[Route('/memory_game', name: 'jeu_memory')]
-    public function memory_game(): Response
+    public function memory_game(Request $request): Response
     {
+
+        /*$pathToHelloWorldScript = ".\games\demineur-easy.py";
+        $process = Process::fromShellCommandline('C:\Users\Anzid\AppData\Local\Programs\Python\Python39\python.exe', $pathToHelloWorldScript);
+        $process->run();
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+       
+        //echo $process->getOutput();*/
+
+
         return $this->render('jeu/memory.html.twig',[
             'controller_name' => 'JeuController',
         ]);
