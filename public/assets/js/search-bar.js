dcb0dc57d23080af9ef8a09eae74883328937fb1
abel <abel.nunes@outlook.fr>
2022-07-28 11:42:36 +0200

    Auto stash before rebase of "louise"

---
+++ public/assets/js/search-bar.js
@@ -0,0 +1,23 @@
+  
+  function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("myDropdown");
+    a = div.getElementsByTagName("a");
+    console.log(filter)
+
+    if(filter !== ''){
+        div.style.display = "block";
+    } else {
+        div.style.display = "none";
+    }
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
