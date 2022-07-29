615cc4988b5ef35e0d1524b4e3cf0c3eea2fe46b
Ayoub Anzid <anzidayoub@outlook.fr>
2022-07-29 13:29:05 +0200

    Abonnement fini

--- src/Controller/Admin/OrderCrudController.php
+++ src/Controller/Admin/OrderCrudController.php
@@ -29,4 +29,5 @@ class OrderCrudController extends AbstractCrudController
             BooleanField::new('IsPaid')
         ];
     }
+    
 }
--- src/Controller/SubscribeCartController.php
+++ src/Controller/SubscribeCartController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Classe\Cart;
+use App\Classe\SubscribeCart;
 use App\Entity\Abonnement;
     
 class SubscribeCartController extends AbstractController
@@ -19,18 +19,18 @@ class SubscribeCartController extends AbstractController
             $this->entityManager = $entityManager;
         }
     
-        #[Route('/panier', name: 'cart')]
-        public function index(Cart $cart)
+        #[Route('/panier-abonnement', name: 'cart')]
+        public function index(SubscribeCart $cart)
         {
             return $this->render('cart/subscribe.html.twig', [
-                "cart" => $cart->getFull(),
+                "cart" => $cart->getFullSubscribe(),
             ]);
         }
     
     
     
         #[Route('/cart/add/{id}', name: 'cart_to_add')]
-        public function add(Cart $cart , $id)
+        public function add(SubscribeCart $cart , $id)
         {
             
             $cart->add($id);
@@ -38,22 +38,22 @@ class SubscribeCartController extends AbstractController
         }
     
        
-        #[Route('/cart/remove', name: 'cart_t_remove')]
-        public function remove(Cart $cart)
+        #[Route('/cart/remove', name: 'cart_to_remove')]
+        public function remove(SubscribeCart $cart)
         {
             $cart->remove();
             return $this->redirectToRoute('cart');
         }
     
         #[Route('/cart/delete/{id}', name: 'cart_to_delete')]
-        public function delete(Cart $cart, $id)
+        public function delete(SubscribeCart $cart, $id)
         {
             $cart->delete($id);
             return $this->redirectToRoute('cart');
         }
     
         #[Route('/cart/decrease/{id}', name: 'decrease')]
-        public function decrease(Cart $cart, $id)
+        public function decrease(SubscribeCart $cart, $id)
         {
             $cart->decrease($id);
             return $this->redirectToRoute('cart');
@@ -61,6 +61,3 @@ class SubscribeCartController extends AbstractController
     }
     
 
-
-    
-}
